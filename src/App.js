1a427ddea5f01885b036dc519e553eee0aa7a16c
Arham Irfan <arham.irfan@logicon.com.pk>
2023-01-09 16:05:56 +0500

    testing1

--- src/App.js
+++ src/App.js
@@ -1,24 +1,49 @@
-import logo from './logo.svg';
-import './App.css';
-
+import React, { useState } from "react";
+import Web3 from "web3";
 function App() {
+  const [account, setAccount] = useState('');
+  let walletConnection = async () => {
+    try {
+      if (typeof window.ethereum !== 'undefined') {
+        let ethereum = window.ethereum
+        let web3 = new Web3(ethereum);
+        if (web3.currentProvider.isMetaMask) {
+          let accounts = await window.ethereum
+            .request({
+              method: "eth_requestAccounts",
+            })
+          console.log('accountByIsMetaMask', accounts)
+          setAccount(accounts[0])
+        } else {
+          web3.currentProvider.openMetaMask().then(async () => {
+            let accounts = await window.ethereum.request({ method: "eth_requestAccounts", })
+            console.log('accountByOpenMetaMask', accounts)
+            setAccount(accounts[0])
+          });
+        }
+      } else {
+        console.log('typeOf windowEth undefined')
+      }
+    } catch (e) {
+      console.log('error:', e);
+    }
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div>
+        <button onClick={walletConnection}>connect to metamask</button>
+      </div>
+
+      <div>
+        {account ? (
+          <p>Your Ethereum address: {account}</p>
+        ) : (
+          <p>Wallet not connected</p>
+        )}
+      </div>
+    </>
+
   );
 }
 
